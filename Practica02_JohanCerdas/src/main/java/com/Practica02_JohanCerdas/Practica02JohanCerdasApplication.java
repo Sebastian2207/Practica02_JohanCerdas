c2d77f8486dbd9b2df137b7cd665936b604dd135
Sebastian Cerdas <70732029+Sebastian2207@users.noreply.github.com>
2023-02-06 18:05:26 -0600

    Creación del proyecto

---
+++ Practica02_JohanCerdas/src/main/java/com/Practica02_JohanCerdas/Practica02JohanCerdasApplication.java
@@ -0,0 +1,13 @@
+package com.Practica02_JohanCerdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica02JohanCerdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica02JohanCerdasApplication.class, args);
+	}
+
+}
---
+++ Practica02_JohanCerdas/src/test/java/com/Practica02_JohanCerdas/Practica02JohanCerdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica02_JohanCerdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica02JohanCerdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
